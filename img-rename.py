186ed854bb3e364f7deaf9db6dc8d93d7d083c3b
Alexander Miller <alexander@Alexs-MacBook-Pro.local>
2019-04-19 12:32:43 -0700

    Initial Commit - PyQT

--- img-rename.py
+++ img-rename.py
@@ -1,79 +1,141 @@
-import tkinter as tk
-from tkinter import filedialog
-
+import sys
+from PyQt5.QtWidgets import *
 import os
 import csv
 
-imgFileNames = [];
+class CSVGroup(QGroupBox):
+    def __init__(self, parent=None):
+        super(CSVGroup, self).__init__(parent)
+        self.parent = parent
+        self.imgFileNames = []
+        self.nameDuplicates = []
+        self.csvBox = QVBoxLayout()
+        self.createDisplayTable()
+        self.createUploadButton()
+        self.setLayout(self.csvBox)
 
-def readFile(file):
-    with open(file, mode='r') as csv_file:
+    def createUploadButton(self):
+        self.csvUploadButton = QPushButton("Import CSV")
+        self.csvUploadButton.clicked.connect(self.csvUpload)
+        self.csvBox.addWidget(self.csvUploadButton)
+
+    def createDisplayTable(self):
+        self.nameTable = QTableWidget(10, 2)
+        self.nameTable.setHorizontalHeaderLabels(["Old Name", "New Name"])
+        self.nameTable.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
+        self.nameTable.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
+        self.csvBox.addWidget(self.nameTable)
+
+    def csvUpload(self):
+        csvFile = QFileDialog.getOpenFileName(filter = "csv(*.csv)")[0]
+        try:
+            self.readCSV(csvFile)
+            self.displayNames()
+        except(err):
+            print(err)
+            return
+
+    def readCSV(self, csvFile):
+        with open(csvFile, mode='r') as csv_file:
             reader = csv.reader(csv_file)
             index = 1
             for row in reader:
-                if any(row[0] in sublist for sublist in imgFileNames):
-                    print(str(index) + ': ' + row[0])
+                fileNames = [row[0], row[1]]
+                if any(row[0] in sublist for sublist in self.imgFileNames):
+                    self.nameDuplicates.append(fileNames)
                 else:
-                    fileNames = [row[0], row[1]]
-                    imgFileNames.append(fileNames)
+                    self.imgFileNames.append(fileNames)
                 index = index + 1
 
-def csvUpload():
-    fname = filedialog.askopenfilename(initialdir = "/Downloads", title = "Select CSV file")
-    try:
-        readFile(fname)
-    except:                     # <- naked except is a bad idea
-            print("Open Source File", "Failed to read file\n'%s'" % fname)
-    return
+    def displayNames(self):
+        self.alertParent()
+        self.nameTable.setRowCount(len(self.imgFileNames))
+        row = 0
+        for imgNames in self.imgFileNames:
+            self.nameTable.setItem(row, 0, QTableWidgetItem(imgNames[0]))
+            self.nameTable.setItem(row, 1, QTableWidgetItem(imgNames[1]))
+            row = row + 1
 
-def readFolder(folder):
-    skipped = ''
-    for root, dirs, files in os.walk(folder):
-        for filename in files:
-            renamed = False
-            for nameTouple in imgFileNames:
-                if nameTouple[0].lower() == filename.lower() and nameTouple[0].lower() != nameTouple[1].lower():
-                    renamed = True
-                    os.rename(os.path.join(root, filename), os.path.join(root, nameTouple[1]));
-            if renamed == False:
-                skipped = skipped + filename + '\n'
-    print(skipped);
+    def alertParent(self):
+        self.parent.handleCSV(self.imgFileNames)
 
-def imgFolderUpload():
-    fname = filedialog.askdirectory(initialdir = "/Downloads", title = "Select folder")
-    if fname:
-            try:
-                readFolder(fname)
-            except Exception as e:                     # <- naked except is a bad idea
-                print(e)
-            return
+class FolderGroup(QGroupBox):
+    def __init__(self, parent=None):
+        super(FolderGroup, self).__init__(parent)
+
+        self.parent = parent
+        self.folder = None
+        self.createWidgets();
 
 
-root = tk.Tk()
-frame = tk.Frame(root)
-frame.pack()
+    def createWidgets(self):
+        verticalBox = QVBoxLayout()
+        verticalBox.addWidget(self.scrollBox())
+        verticalBox.addWidget(self.folderUploadButton())
+        self.setLayout(verticalBox)
 
-csvFile = tk.Button(frame,
-                   text="Select CSV",
-                   bg="green",
-                   padx = 100,
-                   command=csvUpload)
-csvFile.pack(side=tk.LEFT)
+    def scrollBox(self):
+        scroll = QScrollArea()
+        widget = QWidget()
+        grid = QGridLayout(widget)
+        if self.folder == None:
+            row = 0
+            for filenames in self.parent.imgNames:
+                print(row)
+                label = QLabel("No Folder Selected")
+                entry = QLineEdit(filenames[0])
+                grid.addWidget(label, row, 0)
+                grid.addWidget(entry, row, 1)
+                row = row + 1
+        scroll.setWidget(widget)
+        scroll.setWidgetResizable(True)
+        return scroll
+
+    def folderUploadButton(self):
+        button = QPushButton("Select Folder")
+        button.clicked.connect(self.folderUpload)
+        return button
+
+    def folderUpload(self):
+        print('test')
 
 
-imgFolder = tk.Button(frame,
-                   text="Select Folder",
-                   bg="green",
-                   padx = 100,
-                   command=imgFolderUpload)
-imgFolder.pack(side=tk.LEFT)
 
-exit = tk.Button(frame,
-                   text="Quit",
-                   bg="green",
-                   padx = 100,
-                   command=quit)
-exit.pack(side=tk.RIGHT)
+class Window(QWidget):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+        self.text = "test"
+        self.setGeo()
+        self.createWidgets()
+        self.show()
 
 
-root.mainloop()
+    def setGeo(self):
+        # self.resize(800, 800)
+        self.setMinimumHeight(500)
+        self.setMinimumWidth(800)
+        self.grid = QGridLayout()
+        # self.grid.setRowStretch(1, 1)
+        # self.grid.setRowStretch(2, 1)
+        # self.grid.setColumnStretch(0, 1)
+        # self.grid.setColumnStretch(1, 1)
+        self.setLayout(self.grid)
+        self.setWindowTitle("Image Rename")
+
+
+
+    def createWidgets(self):
+        self.csvGroup = CSVGroup(self)
+        self.grid.addWidget(self.csvGroup, 0, 0)
+
+    def handleCSV(self, names):
+        self.imgNames = names
+        self.folderGroup = FolderGroup(self)
+        self.grid.addWidget(self.folderGroup, 0, 1)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Window()
+    sys.exit(app.exec_())
